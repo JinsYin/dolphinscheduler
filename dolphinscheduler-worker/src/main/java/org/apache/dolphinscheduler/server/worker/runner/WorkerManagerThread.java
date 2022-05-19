4e9abd4ec341f2c5eb14ca1412698b2a05caf508
JinsYin <jinsyin@gmail.com>
2022-05-19 11:03:27 +0800

    3.0.0-alpha

---
+++ dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/runner/WorkerManagerThread.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.dolphinscheduler.server.worker.runner;
+
+import org.apache.dolphinscheduler.common.enums.Event;
+import org.apache.dolphinscheduler.common.storage.StorageOperate;
+import org.apache.dolphinscheduler.common.thread.Stopper;
+import org.apache.dolphinscheduler.common.thread.ThreadUtils;
+import org.apache.dolphinscheduler.plugin.task.api.TaskExecutionContext;
+import org.apache.dolphinscheduler.plugin.task.api.TaskExecutionContextCacheManager;
+import org.apache.dolphinscheduler.plugin.task.api.enums.ExecutionStatus;
+import org.apache.dolphinscheduler.remote.command.TaskExecuteResponseCommand;
+import org.apache.dolphinscheduler.server.worker.cache.ResponseCache;
+import org.apache.dolphinscheduler.server.worker.config.WorkerConfig;
+import org.apache.dolphinscheduler.server.worker.processor.TaskCallbackService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Manage tasks
+ */
+@Component
+public class WorkerManagerThread implements Runnable {
+
+    private final Logger logger = LoggerFactory.getLogger(WorkerManagerThread.class);
+
+    /**
+     * task queue
+     */
+    private final DelayQueue<TaskExecuteThread> workerExecuteQueue = new DelayQueue<>();
+
+    @Autowired(required = false)
+    private StorageOperate storageOperate;
+
+    /**
+     * thread executor service
+     */
+    private final ExecutorService workerExecService;
+
+    /**
+     * task callback service
+     */
+    @Autowired
+    private TaskCallbackService taskCallbackService;
+
+    public WorkerManagerThread(WorkerConfig workerConfig) {
+        workerExecService = ThreadUtils.newDaemonFixedThreadExecutor("Worker-Execute-Thread", workerConfig.getExecThreads());
+    }
+
+    /**
+     * get delay queue size
+     *
+     * @return queue size
+     */
+    public int getDelayQueueSize() {
+        return workerExecuteQueue.size();
+    }
+
+    /**
+     * get thread pool queue size
+     *
+     * @return queue size
+     */
+    public int getThreadPoolQueueSize() {
+        return ((ThreadPoolExecutor) workerExecService).getQueue().size();
+    }
+
+    /**
+     * Kill tasks that have not been executed, like delay task
+     * then send Response to Master, update the execution status of task instance
+     */
+    public void killTaskBeforeExecuteByInstanceId(Integer taskInstanceId) {
+        workerExecuteQueue.stream()
+                          .filter(taskExecuteThread -> taskExecuteThread.getTaskExecutionContext().getTaskInstanceId() == taskInstanceId)
+                          .forEach(workerExecuteQueue::remove);
+        sendTaskKillResponse(taskInstanceId);
+    }
+
+    /**
+     * kill task before execute , like delay task
+     */
+    private void sendTaskKillResponse(Integer taskInstanceId) {
+        TaskExecutionContext taskExecutionContext = TaskExecutionContextCacheManager.getByTaskInstanceId(taskInstanceId);
+        if (taskExecutionContext == null) {
+            return;
+        }
+        TaskExecuteResponseCommand responseCommand = new TaskExecuteResponseCommand(taskExecutionContext.getTaskInstanceId(), taskExecutionContext.getProcessInstanceId());
+        responseCommand.setStatus(ExecutionStatus.KILL.getCode());
+        ResponseCache.get().cache(taskExecutionContext.getTaskInstanceId(), responseCommand.convert2Command(), Event.RESULT);
+        taskCallbackService.sendTaskExecuteResponseCommand(taskExecutionContext);
+    }
+
+    /**
+     * submit task
+     *
+     * @param taskExecuteThread taskExecuteThread
+     * @return submit result
+     */
+    public boolean offer(TaskExecuteThread taskExecuteThread) {
+        return workerExecuteQueue.offer(taskExecuteThread);
+    }
+
+    public void start() {
+        Thread thread = new Thread(this, this.getClass().getName());
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        Thread.currentThread().setName("Worker-Execute-Manager-Thread");
+        TaskExecuteThread taskExecuteThread;
+        while (Stopper.isRunning()) {
+            try {
+                taskExecuteThread = workerExecuteQueue.take();
+                taskExecuteThread.setStorageOperate(storageOperate);
+                workerExecService.submit(taskExecuteThread);
+            } catch (Exception e) {
+                logger.error("An unexpected interrupt is happened, "
+                    + "the exception will be ignored and this thread will continue to run", e);
+            }
+        }
+    }
+}
